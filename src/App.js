17ed5f98eda13fc5943e73f151ef472b8dc8500b
deepakkarki <deepak6k@gmail.com>
2017-08-14 23:49:50 +0530

    Added listing and search(form) - next step is splitting up the app component into '<Search>' and '<Table>' components.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,78 @@
 import React, { Component } from 'react'; 
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://facebook.github.io/react/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+}, {
+    title: 'Redux',
+    url: 'https://github.com/reactjs/redux',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+}, ];
 
+
+function isSearched(searchTerm) {
+  return function(item) {
+    return !searchTerm ||
+      item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  } 
+}
 
 class App extends Component { 
+
+  constructor(props){
+    super(props);
+    this.state = {
+      list: list,
+      searchTerm: ""
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this)
+  }
+
+  onDismiss(id){
+    const updatedList = this.state.list.filter(item => item.objectID !== id);
+    this.setState({ list: updatedList });
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
   render() {
-    var helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        <form>
+          <input type="text" onChange={this.onSearchChange}/> 
+        </form>
+        {
+          this.state.list
+            .filter(isSearched(this.state.searchTerm))
+            .map(item => {
+              return (
+                <div key={item.objectID} className="item">
+                  <span>
+                    <a href={item.url}> {item.title} </a>
+                  </span>
+                  <span> {item.author} </span>
+                  <span> {item.num_comments} </span>
+                  <span> {item.points}  </span>
+                  <span>
+                    <button onClick={() => this.onDismiss(item.objectID)} 
+                      type="button"> Dismiss </button>
+                  </span>
+                </div>
+              );
+            })
+        }
       </div>
     ); 
   }
